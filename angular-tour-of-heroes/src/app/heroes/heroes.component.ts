b10b45de55328bf68c16a5c2d4d76f7a325a33c1
Wyat Soule <wsoule679@gmail.com>
2022-03-26 16:09:59 -0400

    add not interactable list of heroes

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heros'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,10 +8,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
 
   constructor() { }
 
---
+++ angular-tour-of-heroes/src/app/mock-heros.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice'},
+    { id: 12, name: 'Yellow'},
+    { id: 13, name: 'Dark'},
+    { id: 14, name: 'Bright'},
+    { id: 15, name: 'Indecent'},
+    { id: 16, name: 'Mrs Warm'},
+    { id: 17, name: 'Ms Cold'},
+    { id: 18, name: 'Mr Hot'},
+    { id: 19, name: 'Dr Bad'}
+]
