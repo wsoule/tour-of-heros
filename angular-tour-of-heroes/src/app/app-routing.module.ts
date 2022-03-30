a6590ab0737ce9e3892f0a044d536d84f9dc52d9
Wyat Soule <wsoule679@gmail.com>
2022-03-29 21:38:53 -0400

    update routing module

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent},
-  { path: '', redirectTo: 'dashboard', pathMatch:'full'}
+  { path: '', redirectTo: 'dashboard', pathMatch:'full'},
 ];
 
 @NgModule({
