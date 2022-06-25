4063b36081ecbd605c044c9c3ca4c99ceb4301ff
Wyat Soule <wsoule679@gmail.com>
2022-06-25 18:36:52 -0400

    update package-lock

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
